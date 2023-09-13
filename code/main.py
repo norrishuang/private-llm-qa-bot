65adf4a2272f7fcad1886a7853c184075db3eeec
xiohuang <xiohuang@amazon.com>
2023-09-13 20:16:38 +0800

    debug

--- code/main.py
+++ code/main.py
@@ -325,6 +325,8 @@ class CustomDocRetriever(BaseRetriever,BaseModel):
             '''
             def get_topk_items(opensearch_knn_respose, opensearch_query_response, topk=1):
 
+                logger.info(f"recall records(knn): {str(opensearch_knn_respose.count())}")
+                logger.info(f"recall records(query): {str(opensearch_query_response.count())}")
                 opensearch_knn_nodup = []
                 unique_ids = set()
                 for item in opensearch_knn_respose:
